88b212fb218cea4bebdb04bebde33b60f64cf040
Mateusz Jagiełło <sigo@sigo.pl>
2018-08-18 23:58:24 +0200

    Testing CI

--- build.sh
+++ build.sh
@@ -3,13 +3,20 @@
 URL="https://sjp.pl/"
 DICTIONARIES_LIST_URL="${URL}slownik/en/"
 RESULT_FILE="${RESULT_DIR}/pl.txt" # RESULT_DIR is environment variable, e.g. set by CI
-
-# TODO: Check availability `RESULT_DIR` variable
+MIN_WORDS=1000000 # Dictionary must have greater than MIN_WORDS words, or it is damaged
 
 print_text() {
     echo
     echo "# ${1}"
 }
+
+print_error() {
+    echo "${1}"
+    exit 1
+}
+
+[ -z "${RESULT_DIR}" ] && print_error "RESULT_DIR variable is not set"
+[ ! -d "${RESULT_DIR}" ] && print_error "RESULT_DIR directory doesn't exists"
 
 print_text "Downloading list of dictionaries"
 DICTIONARY_PATH=$(wget -qO - "${DICTIONARIES_LIST_URL}" | sed -n 's/^.*"\/\(.*sjp-aspell6.*\.tar\.bz2\)".*$/\1/p')
@@ -18,10 +25,10 @@ DICTIONARY_URL="${URL}${DICTIONARY_PATH}"
 DICTIONARY_FILENAME=$(echo "${DICTIONARY_PATH}" | sed -n 's/^.*\(sjp-aspell6.*\.tar\.bz2\)$/\1/p')
 DICTIONARY_NAME=$(echo "${DICTIONARY_FILENAME}" | sed -n 's/^sjp-\(.*\)\.tar\.bz2$/\1/p')
 
-print_text "Downloading dictionary file"
+print_text "Downloading dictionary file (${DICTIONARY_URL})"
 wget -qO "${DICTIONARY_FILENAME}" "${DICTIONARY_URL}"
 
-print_text "Extracting dictionary file"
+print_text "Extracting dictionary file (${DICTIONARY_FILENAME})"
 tar xvjf "${DICTIONARY_FILENAME}"
 
 print_text "Installing dictionary"
@@ -39,8 +46,10 @@ aspell -d pl dump master | \
     uniq | \
     sort > "${RESULT_FILE}"
 
-# TODO: Check file exists
-# TODO: Check possibility damage dictionary
+[ ! -f "${RESULT_FILE}" ] && print_error "Dictionary file doesn't exists"
 
 NUMBER_OF_WORDS=$(wc -l "${RESULT_FILE}" | cut -d' ' -f1)
+
+[ "${NUMBER_OF_WORDS}" -lt "${MIN_WORDS}" ] && print_error "Dictionary is damaged"
+
 print_text "Generated dictionary have ${NUMBER_OF_WORDS} words"
