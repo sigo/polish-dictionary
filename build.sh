ab60613f59238e73f1139c59d2057116a22b78c7
Mateusz Jagiełło <sigo@sigo.pl>
2018-08-18 23:19:40 +0200

    Testing CI

--- build.sh
+++ build.sh
@@ -2,34 +2,36 @@
 
 URL="https://sjp.pl/"
 DICTIONARIES_LIST_URL="${URL}slownik/en/"
-RESULT_FILE="dist/pl.txt"
+RESULT_FILE="${RESULT_DIR}/pl.txt" # RESULT_DIR is environment variable, e.g. set by CI
 
-echo
-echo "Downloading list of dictionaries"
+# TODO: Check availability `RESULT_DIR` variable
+
+print_text() {
+    echo
+    echo "# ${1}"
+}
+
+print_text "Downloading list of dictionaries"
 DICTIONARY_PATH=$(wget -qO - "${DICTIONARIES_LIST_URL}" | sed -n 's/^.*"\/\(.*sjp-aspell6.*\.tar\.bz2\)".*$/\1/p')
 DICTIONARY_URL="${URL}${DICTIONARY_PATH}"
 
 DICTIONARY_FILENAME=$(echo "${DICTIONARY_PATH}" | sed -n 's/^.*\(sjp-aspell6.*\.tar\.bz2\)$/\1/p')
 DICTIONARY_NAME=$(echo "${DICTIONARY_FILENAME}" | sed -n 's/^sjp-\(.*\)\.tar\.bz2$/\1/p')
 
-echo
-echo "Downloading dictionary file"
+print_text "Downloading dictionary file"
 wget -qO "${DICTIONARY_FILENAME}" "${DICTIONARY_URL}"
 
-echo
-echo "Extracting dictionary file"
+print_text "Extracting dictionary file"
 tar xvjf "${DICTIONARY_FILENAME}"
 
-echo
-echo "Installing dictionary"
+print_text "Installing dictionary"
 cd "${DICTIONARY_NAME}"
 ./configure
 make
 make install
 cd ..
 
-echo
-echo "Dumping dictionary to file"
+print_text "Dumping dictionary to file"
 aspell -d pl dump master | \
     aspell -l pl expand | \
     sed 's/\s\s*/\n/g' | \
@@ -37,6 +39,8 @@ aspell -d pl dump master | \
     uniq | \
     sort > "${RESULT_FILE}"
 
+# TODO: Check file exists
+# TODO: Check possibility damage dictionary
+
 NUMBER_OF_WORDS=$(wc -l "${RESULT_FILE}" | cut -d' ' -f1)
-echo
-echo "Generated dictionary have ${NUMBER_OF_WORDS} words"
+print_text "Generated dictionary have ${NUMBER_OF_WORDS} words"
