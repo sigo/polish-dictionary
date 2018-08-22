74a3e4d8595619b29833afb19dc27111f7e98b0b
Mateusz Jagiełło <sigo@sigo.pl>
2018-08-22 23:05:53 +0200

    Testing CI

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,10 +6,10 @@ RESET_COLORS="\e[0m"
 
 print_text() {
     echo
-    echo "${LIGHT_GREEN_BOLD}${1}${RESET_COLORS}"
+    echo -e "${LIGHT_GREEN_BOLD}${1}${RESET_COLORS}"
 }
 
 print_error() {
-    echo "${LIGHT_RED_BOLD}${1}${RESET_COLORS}"
+    echo -e "${LIGHT_RED_BOLD}${1}${RESET_COLORS}"
     exit 1
 }
