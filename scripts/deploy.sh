506c0eb0380ed80d8835c8943f849b5ecbde76e2
Mateusz Jagiełło <sigo@sigo.pl>
2018-09-26 20:16:50 +0200

    Testing CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,7 @@ if [ -n "$(git diff)" ]; then
     README="README.md"
     ENC_SSH_KEY="./id_rsa.enc"
     REPO_URL="git@github.com:sigo/polish-dictionary.git"
+    COMMIT_MESSAGE="Update [skip ci]"
 
     print_text "Generating readme file"
     cat "${README_TEMPLATE}" | \
@@ -24,29 +25,30 @@ if [ -n "$(git diff)" ]; then
     print_text "Pushing updates to repository"
 
     # Start SSH agent
-    eval `ssh-agent -s`
+    eval `ssh-agent -s` &> /dev/null
 
     # Decrypt SSH key for deployment to GitHub and add it to agent
     openssl aes-256-cbc \
         -K "${encrypted_4abc9283474e_key}" \
         -iv "${encrypted_4abc9283474e_iv}" \
         -in "${ENC_SSH_KEY}" -d | \
-        ssh-add -
+        ssh-add - \
+        &> /dev/null
 
     # Change origin's URL to ssh-authenticated (initial clone is https)
     git remote set-url origin "${REPO_URL}"
 
     # Checkout to parent of detached branch
-    git checkout -
+    git checkout - &> /dev/null
 
     # Add files as tracked
     git add .
 
     # Commit changes
-    git commit -m "Update [skip ci]"
+    git commit -m "${COMMIT_MESSAGE}"
 
     # Push changes
-    git push
+    git push &> /dev/null
 else
     print_text "Everything is up-to-date"
 fi
